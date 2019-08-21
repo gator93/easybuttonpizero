12ab258fd10059a9540b466dede3d72f973db66d
Dave McKienzie <dmckie@gmail.com>
2019-08-21 08:57:25 -0400

    initial code

---
+++ listen-for-shutdown.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import subprocess
+import os.path
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.wait_for_edge(3, GPIO.FALLING)
+
+os.mknod('/home/pi/easy/stop')
+
+subprocess.call(['shutdown', '-h', 'now'], shell=False)
